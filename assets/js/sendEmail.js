31f9a3d8d820315112f58d6f84635171cd18051d
Clifford Laserre <claserre9@gmail.com>
2020-10-15 17:21:50 -0400

    Update sendEmail.js

--- assets/js/sendEmail.js
+++ assets/js/sendEmail.js
@@ -1,18 +1,23 @@
-function sendEmail() { 
+function sendEmail(expeditor, message) { 
     Email.send({
-        Host : "smtp.yourisp.com",
-        Username : "username",
-        Password : "password",
-        To : 'them@website.com',
-        From : "you@isp.com",
-        Subject : "This is the subject",
-        Body : "And this is the body"
+        Host : "smtp.gmail.com",
+        Username : "claserre9",
+        Password : "jctqkgzfpkzvewgs",
+        To : 'talmag9@gmail.com',
+        From : expeditor,
+        Subject : "Message from github.io",
+        Body : message
     }).then(
         swal("Good job!", "You clicked the button!", "success")
     );
 } 
 
-$( "#contactForm" ).submit(function( e ) {
-    swal("Good job!", "You clicked the button!", "success")
+$( "#contactForm" ).submit(function(e) {
+    const data = $( this ).serializeArray()
+    const expeditor = `${data[0].value} (Email : ${data[1].value} - Phone : ${data[2].value})`
+    const message = `${data[3].value}`
+    sendEmail(expeditor, message)
+    swal("Thanks!", "Your message has been sent", "success")
+    $(this).reset();
     e.preventDefault();
   });
